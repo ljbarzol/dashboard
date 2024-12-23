d3925c1459a101f3bdbb1ca8e5fb95e3543280a0
ljbarzol <ljbarzol@espol.edu.ec>
2024-12-22 22:10:40 -0500

    Updates

--- src/interface/item.tsx
+++
@@ -1,8 +0,0 @@
-export default interface Item {
-    dateStart: string | null;
-    dateEnd:string | null;
-    precipitation:string; 
-    humidity: string; 
-    clouds: string
-    temperature: string
-}
